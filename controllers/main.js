a6f62c73dfd53e89c8333d73c907398822d298a9
romanKrupnov <89263797575@rambler.ru>
2020-11-25 15:12:11 +0300

    1) С помощью созданной в предыдущем уроке модели работы с объектами на сервере создайте REST API для просмотра данных с помощью метода GET. 2) Добавьте в приложение возможность импорта данных с помощью POST, модификации данных с помощью PUT и их удаления с помощью DELETE.

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,4 @@
-// exports.indexPage = (req, res, next) => {
-//     res.render('index', { title: 'Task App', message: 'Hello, World!' })
-// }
+
 exports.indexPage = (req, res, next) => {
     res.json({Title: 'Task App', Message: 'Hello, World!', Status: 200});
 }
